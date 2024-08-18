be54c863c1c8c8e5f48456df2a00afb34e409265
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-08-18 11:49:43 +0530

    create oop of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummmary.js";
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
-
+import '../cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
