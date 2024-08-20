3031fbdf5288cfae8cb7e8d16ddeccc8c7097b9e
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-08-20 19:56:48 +0530

    Load products fro the backend

--- data/products.js
+++ data/products.js
@@ -28,7 +28,7 @@ class Product{
     return `images/ratings/rating-${this.rating.stars * 10}.png`
   }
   getPrice(){
-    return `$${formatCurrency(this.priceCents /100)}`
+    return `$${formatCurrency(this.priceCents)}`
   }
   extraInfoHTML(){
     return '';
@@ -51,8 +51,26 @@ class Clothing extends Product{
 }
 
 
+export let products=[];
+
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', ()=>{
+    products=JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type==='clothing') return new Clothing(productDetails);
+      return new Product(productDetails);
+    });
+    console.log('load products');
+
+    fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
 
 
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -716,3 +734,4 @@ export const products = [
   if(productDetails.type==='clothing') return new Clothing(productDetails);
   return new Product(productDetails);
 });
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,12 @@
 import { renderOrderSummary } from './checkout/orderSummmary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
-import '../cart-class.js';
-import '../data/backend-practice.js'
 
-renderOrderSummary();
-renderPaymentSummary();
+import { loadProducts } from '../data/products.js';
+    //import '../cart-class.js';
+//import '../data/backend-practice.js'
+
+
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
