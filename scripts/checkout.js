f631cadccd04e5c861d15eaf595d9578c930e4fb
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-08-20 21:06:41 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,30 @@ import { renderOrderSummary } from './checkout/orderSummmary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
 
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
     //import '../cart-class.js';
 //import '../data/backend-practice.js'
 
 
+
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1');
+    });
+}).then((value)=>{
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
